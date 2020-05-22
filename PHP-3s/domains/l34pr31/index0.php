cd6e8ab3113502b60343adb4938ff7fdfc5dbd59
itvafrolov <itvafrolov@gmail.com>
2020-05-22 09:30:16 +0300

    Create index0.php

---
+++ PHP-3s/domains/l34pr31/index0.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<p, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Привет!!!</p>
+    <!-- PHP -->    
+    <?php
+    //Однострочный комментарий 
+    # Однострочный комментарий
+    /* Многострочный 
+    комментарий */
+        echo "<p> ПРИВЕТ из РНР!!!  --2 </p>";
+        //
+    ?>
+    <?php
+        echo "<p> ПРИВЕТ из РНР!!!  --3 </p>";
+        echo "<p> ПРИВЕТ из РНР!!!  --3-2 </p>";
+    ?>
+
+    <?php
+    
+    ?>
+
+</body>
+</html>
