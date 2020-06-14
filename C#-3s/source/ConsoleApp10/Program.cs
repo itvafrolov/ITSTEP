44a30505d956ec5d6e124f94f49ef2fc8763e89d
itvafrolov <itvafrolov@gmail.com>
2020-06-14 20:03:58 +0300

    1

---
+++ C#-3s/source/ConsoleApp10/Program.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ интерфейсы - это специальная конструкция, (стандартные интерфейсы вообще их много) - содержащая в себе набор методов, 
+не методов реализации а описательная как прототип в С++ класс, в который включен интерфейс должен реализовать все методы этого интерфейса
+ */
+
+namespace ConsoleApp10
+{
+    interface IFly
+    {
+        //нельзя указывать модификаторы доступа
+        void Fly();
+    }
+    interface ISwim
+    {
+        void Swim();
+    }
+    interface IRun
+    {
+        void Run(int speed);
+    }
+    abstract class Bird
+    {
+        //  public abstract void Fly();
+    }
+    class Duck : Bird, IFly
+    {
+        //....
+        //public override void Fly()
+        //{
+        //    Console.WriteLine("Duck fly");
+        //}
+        //public void Swit()
+        //{
+        //    Console.WriteLine("Duck swit");
+        //}
+        public void Fly()
+        {
+            Console.WriteLine("Duck fly");
+        }
+    }
+    class Ostrin : Bird, IRun
+    {
+        public void Run(int speed)
+        {
+            Console.WriteLine($"Ostrin run {speed} km/ch");
+        }
+    }
+    class Penguin : Bird, ISwim
+    {
+        public void Fly()
+        {
+            // Console.WriteLine("Penguin swit");
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("Penguin swit");
+        }
+
+
+    }
+    /*
+     ВОПРОСЫ:
+        в одном файле сколько интерфейсов? один или много?
+     */
+
+    abstract class Insect //насекомые
+    {
+
+    }
+
+    class Butterfly : Insect, IFly
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Butterfly fly");
+        }
+
+        public void Ex()
+        {
+            //
+        }
+    }
+
+    class Plane : IFly
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Plane fly");
+        }
+    }
+
+
+    abstract class Human
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public override string ToString()
+        {
+            return $"\nФамилия: {LastName} Имя: {FirstName} Дата рождения: {BirthDate.ToLongDateString()}";
+        }
+    }
+    abstract class Employee : Human
+    {
+        public string Position { get; set; }
+        public double Salary { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"\nДолжность: {Position} Заработная плата: {Salary} $";
+        }
+    }
+
+    interface IWorker
+    {
+        bool IsWorking { get; }
+        string Work();
+    }
+
+    interface IManager
+    {
+        IWorker[] ListOfWorkers { get; set; }
+        void Organize();
+        void Contol();
+        void MakeBudget();
+
+    }
+
+    class Director : Employee, IManager
+    {
+        public IWorker[] ListOfWorkers { get; set; }
+
+        public void Contol()
+        {
+            Console.WriteLine("Contol");
+        }
+
+        public void MakeBudget()
+        {
+            Console.WriteLine("MakeBudget");
+        }
+
+        public void Organize()
+        {
+            Console.WriteLine("Organize");
+        }
+    }
+
+    class Cashier : Employee, IWorker
+    {
+        public bool IsWorking { get; set; } // исходя из реализации данного интерфейса get обязателен set - опционально можно так, можно и без
+        //public bool IsWorking => throw new NotImplementedException();
+
+        public string Work()
+        {
+            return "Cashier works";
+        }
+    }
+
+
+    class Loader : Employee, IWorker
+    {
+        public bool IsWorking { get; }
+
+        public string Work()
+        {
+            return "Loader works";
+        }
+    }
+
+    // наследование интерфейсов
+    interface IA
+    {
+        //void A1(int n);
+        //int A2(double b);
+        void show();
+    }
+
+    interface IB
+    {
+        //void b1();
+        void show();
+    }
+
+    interface IC : IA, IB
+    {
+        //bool C1(char s);
+
+    }
+
+    class Example1 //: IC
+    {
+        public void A1(int n)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int A2(double b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void b1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool C1(char s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Example2 : IA, IB
+    {
+        public void show()
+        {
+            Console.WriteLine("Example2");
+        }
+    }
+
+
+
+    class StudentCard
+    {
+        public int Number { get; set; }
+        public string Series { get; set; }
+
+        public override string ToString()
+        {
+            return $"Студенческий билет: {Series} {Number}";
+        }
+    }
+
+    class Student : IComparable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public StudentCard StudentCard { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is Student)
+            {
+                return LastName.CompareTo((obj as Student).LastName);
+            }
+            throw new ArgumentException();
+           // return LastName.CompareTo(obj);
+        }
+
+        public override string ToString()
+        {
+            return $"Фамилия: {LastName}, Имя: {FirstName}, Родился: {BirthDate.ToLongDateString()}, {StudentCard}";
+        }
+    }
+
+    class Auditory : IEnumerable
+    {
+        private Student[] _students =
+            {
+        new Student {
+            FirstName ="John",
+            LastName ="Miller",
+            BirthDate =new DateTime(1997,3,12),
+            StudentCard =new StudentCard { Number=189356, Series="AB" }
+        },
+        new Student {
+            FirstName ="Candice",
+            LastName ="Leman",
+            BirthDate =new DateTime(1998,7,22),
+            StudentCard =new StudentCard { Number=345185, Series="XA" }
+        },
+        new Student {
+            FirstName ="Joey",
+            LastName ="Finch",
+            BirthDate =new DateTime(1996,11,30),
+            StudentCard =new StudentCard { Number=258322, Series="AA" }
+        },
+        new Student {
+            FirstName ="Nicole",
+            LastName ="Taylor",
+            BirthDate =new DateTime(1996,5,10),
+            StudentCard =new StudentCard { Number=513484, Series="AA" }
+        }
+    };
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _students.GetEnumerator();
+        }
+        public void Sort()
+        {
+            Array.Sort(_students);
+        }
+
+    }
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auditory auditory = new Auditory();
+            foreach(Student item in auditory)
+            {
+                Console.WriteLine(item);
+            }
+
+            auditory.Sort();
+        ////    // IFly fly = new IFly();  // - ошибка экземпляр интерфейса создать нельзя!
+        ////    // IFly fly = new Duck();   // можно
+
+
+        ////    Bird[] birds = { new Penguin(), new Duck(), new Ostrin() };
+
+        ////    //foreach (Bird item in birds)
+        ////    //{
+        ////    //    try
+        ////    //    {
+        ////    //        ((IFly)item).Fly();
+        ////    //    }
+        ////    //    catch { }
+        ////    //    // ----- 2 ----------
+        ////    //    IFly fly = item as IFly;
+        ////    //    if (fly != null)
+        ////    //    {
+        ////    //        fly.Fly();
+        ////    //    }
+        ////    //    //----- 3 --------
+        ////    //    if (item is IFly)
+        ////    //    {
+        ////    //        (item as IFly).Fly();
+        ////    //    }
+        ////    //}
+
+        ////    IFly[] flies = { new Butterfly(), new Duck(), new Plane() };
+
+        ////    foreach (IFly item in flies)
+        ////    {
+        ////        item.Fly(); // полиморфизм
+        ////    }
+
+
+        ////    //---------------------------------------------------------------
+        ////    //Director director = new Director();
+        ////    // director.ListOfWorkers[0] = new Cashier(); // ошибка ListOfWorkers - свойство которе не выделяет память под этот массив
+        ////  /*
+        ////   * director.ListOfWorkers = new IWorker[] { new Cashier(), new Loader() }; // можно так
+        ////    foreach(IWorker item in director.ListOfWorkers)
+        ////    {
+        ////        if (item.IsWorking)
+        ////        {
+        ////            Console.WriteLine(item.Work());
+        ////        }
+        ////    }
+        ////    */
+
+            //
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#-3s/source/ConsoleApp9/Program.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        class Point
+        {
+            public int? X {get; set;}
+            public int? Y {get; set;}
+
+        public static bool operator true(Point p)
+            {
+            return p.X != null && p.Y != null; 
+            }
+        public static bool operator false(Point p)
+            {
+                return p.X == null || p.Y == null;
+            }
+
+        }
+
+        abstract class Figure
+        {
+            public abstract void Draw();
+        }
+
+        abstract class Quadrangle : Figure  {  }
+        class Rectangle : Quadrangle
+        {
+            public int Width { get; set; }
+            public int Height { get; set; }
+            public override void Draw()
+            {
+                //Draw;
+            }
+            public override string ToString()
+            {
+                return $"Width: {Width} Height: {Height}";
+            }   
+            public static implicit operator Rectangle(Square s) //implicit ВАЖНО - пишется когда нет потери данных
+            {
+                return new Rectangle { Width = s.Lenght, Height = s.Lenght };
+            }
+
+            public static explicit operator Square(Rectangle rect)
+            {
+                return new Square
+                {
+                    Lenght = rect.Height < rect.Width ? rect.Height : rect.Width
+                };
+            }
+        }
+        class Square : Quadrangle
+        {
+            public int Lenght { get; set; }
+            public override void Draw()
+            {
+                //
+            }
+            public override string ToString()
+            {
+                return $"Lenght: {Lenght}";
+            }
+            public static explicit operator int(Square s)
+            {
+                return s.Lenght;
+            }
+            public static implicit operator Square(int s)
+            {
+                return new Square { Lenght = s};
+            }
+        }
+
+        class Laptop
+        {
+            public string Vendor { get; set; }
+            public double Price { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Vendor} {Price}";
+            }
+        }
+        enum Vendors  {Asus, Sumsung, LG };
+        class Shop
+        {
+            private Laptop[] _laptops;  // потому что тут private  потому нужно писать индексатор
+
+            public Shop(int size)
+            {
+                _laptops = new Laptop[size];
+            }
+            public int Lenght { get { return _laptops.Length; } }
+            public Laptop this[int index]   // вся ниже следующая конструкция называется индексатор
+            {
+                get
+                {
+                    if (index >= 0 && index < Lenght) return _laptops[index];
+                    throw new IndexOutOfRangeException();
+                }
+
+                set
+                {
+                    if (index >= 0 && index < Lenght)
+                        _laptops[index] = value;
+                }
+            }
+            //перегрузка индексатора
+            public Laptop this[string name]
+            {
+                get
+                {
+                    if (Enum.IsDefined(typeof(Vendors), name))
+                    {
+                        int index = (int)Enum.Parse(typeof(Vendors), name);
+                        return _laptops[index];
+                    }
+                    throw new Exception(";)");
+                }
+
+                set 
+                {
+                if (Enum.IsDefined(typeof(Vendors), name))
+                    {
+                        _laptops[(int)Enum.Parse(typeof(Vendors), name)] = value;
+                    }
+                }
+            }
+
+            private int FindByPrice(double price)
+            {
+                for(int i = 0; i<Lenght; i++)
+                {
+                    if(_laptops[i].Price == price)
+                    {
+                        return i;
+                    }
+                }
+                return -1;
+            }
+
+
+            public Laptop this[double price]
+            {
+                get
+                {
+                    int index = FindByPrice(price);
+                    if (index >= 0)
+                    {
+                        return this[index];  //this (это метод - индексатор) используется потому что мы обращаемся к своему уже написанному индексатору
+                    }
+                    //можно записать так:
+                    // return this[FindByPrice(price)];
+                    throw new Exception("Недопустимая стоимость!");
+                }
+
+                set
+                {
+                    int index = FindByPrice(price);
+                    if (index>=0)
+                    {
+                        this[index] = value;  //this используется потому что мы обращаемся к своему уже написанному индексатору
+                    }
+                }
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            //Point p1 = new Point { X = 5, Y = 12 };
+            //Point p2 = new Point { X = null, Y = 3 };
+            //----------------------------------------------------------
+            /*
+            double d = 23.123;
+            // int n = d;  - такая запись - ошибка, а вот так можно:
+            int n = (int)d;
+            double d1 = n;  // целое к вещественному ошибку не выдает!
+            Rectangle rec = new Rectangle { Width = 10, Height = 6 };
+            Square explisSqu = (Square)rec;
+
+            Square sque2 = new Square { Lenght = 10 };
+            Rectangle rec2 = sque2;
+            Console.WriteLine(rec2);
+            Console.WriteLine(explisSqu);
+            int number = 34;
+            Square intSqu2 = number;
+            int n1 = (int)intSqu2;
+            */
+            //-----------------   индексатор   ---------------------------
+            Shop shop1 = new Shop(3);
+            shop1[0] = new Laptop { Vendor = "Asus", Price = 15354.14 };
+            shop1[1] = new Laptop { Vendor = "Sumsung", Price = 17354.14 };
+            shop1[2] = new Laptop { Vendor = "Lg", Price = 11354.14 };
+
+            for (int i =0; i<shop1.Lenght; i++)
+            {
+                Console.WriteLine(shop1[i]);
+            }
+
+            try
+            {
+                shop1[7] = new Laptop { Vendor = "Lg", Price = 11354.14 };
+                Console.WriteLine(shop1[7]);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine(shop1["Asus"]);
+            shop1["Asus"] = new Laptop { Vendor = "HP", Price = 9354.14 };
+            Console.WriteLine(shop1["Asus"]);
+
+            //
+            Console.ReadKey();
+            
+        }
+    }
+}
