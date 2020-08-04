43f8890452823878c5279e3a7349df8006f5a5d9
itvafrolov <itvafrolov@gmail.com>
2020-08-04 07:37:32 +0300

    Create MyFigure.php

---
+++ PHP-3s/domains/HomeWorks/classes/MyFigure.php
@@ -0,0 +1,18 @@
+<?php
+abstract class MyFigure
+{    
+    public function CalcArea()
+    {
+
+    }
+    public function CalсPerimetr()
+    {
+
+    } 
+    public function DrawFigure()
+    {
+
+    }
+}
+
+?>
